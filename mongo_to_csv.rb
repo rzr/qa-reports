ecb68c3a5e2e9cd9f7e8ebfc3290bcca5cf3c1ce
Sami Hangaslammi <sami.hangaslammi@leonidasoy.fi>
2011-03-23 19:29:55 +0200

    fixes to export scripts

--- mongo_to_csv.rb
+++ mongo_to_csv.rb
@@ -3,19 +3,26 @@ require 'mongo'
 
 db = Mongo::Connection.new.db('qadash-db')
 reports_coll = db.collection('reports')
+bugs_coll = db.collection('bugs')
 
 total_cases = {}
+output = []
 
-reports_coll.find().each do |data|
+(1..12).each do |weeknum|
+  reports = reports_coll.find({"weeknum" => weeknum})
+  total_cases = 0
+  reports.each do |r|
+    total_cases += r["total_cases"]
+  end
 
-  weeknum = data["weeknum"]
-  total_cases[weeknum] = 0 if total_cases[weeknum].nil?
-  total_cases[weeknum] += data["total_cases"]
+  bugs = bugs_coll.find({"weeknum" => weeknum}).count()
+  
+  output << [weeknum, total_cases, bugs]
 
 end
-total_cases.each {|key, value| puts "wk#{key} has #{value} total cases" }
 
-
-
-
+puts "Weeknum, Testcases, Bugs"
+output.each do |row|
+  puts "#{row[0]},#{row[1]},#{row[2]}\n"
+end
 
