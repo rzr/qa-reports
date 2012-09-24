b3715bfb4db355f77afdad64dec2a9c3979663a3
Vesa Poikajärvi <vesa.poikajarvi@leonidasoy.fi>
2012-09-24 14:22:34 +0300

    Bugzilla >= 3.6 integrated authentication

--- app/controllers/bugs_controller.rb
+++ app/controllers/bugs_controller.rb
@@ -1,3 +1,6 @@
+require "erb"
+include ERB::Util
+
 class BugsController < ApplicationController
 
   caches_action :fetch_bugzilla_data,
@@ -19,12 +22,38 @@ class BugsController < ApplicationController
     @http.use_ssl     = BUGZILLA_CONFIG['use_ssl']
     @http.verify_mode = OpenSSL::SSL::VERIFY_NONE
     @http.start() do |http|
+
+      cookie = nil
+      # Bugzilla integrated authentication
+      if not BUGZILLA_CONFIG['bugzilla_username'].nil?
+        # Since Bugzilla 3.6 username and password can be given as parameters
+        login_uri = "#{uri}&Bugzilla_login=#{url_encode(BUGZILLA_CONFIG['bugzilla_username'])}&Bugzilla_password=#{url_encode(BUGZILLA_CONFIG['bugzilla_password'])}"
+
+        req = Net::HTTP::Get.new(login_uri)
+        if not BUGZILLA_CONFIG['http_username'].nil?
+          req.basic_auth BUGZILLA_CONFIG['http_username'], BUGZILLA_CONFIG['http_password']
+        end
+
+        response = http.request(req)
+        cookie   = response.to_hash['set-cookie'].collect{|ea|ea[/^.*?;/]}.join(" ")
+
+        # If bugzilla set us a redirect URI, use that instead of the original one
+        if response.header['location']
+          uri = response.header['location']
+        end
+      end
+
       req = Net::HTTP::Get.new(uri)
       if not BUGZILLA_CONFIG['http_username'].nil?
         req.basic_auth BUGZILLA_CONFIG['http_username'], BUGZILLA_CONFIG['http_password']
       end
+
+      if not cookie.nil?
+        req['Cookie'] = cookie
+      end
+
       response = http.request(req)
-      content = response.body
+      content  = response.body
     end
 
     # XXX: bugzilla seems to encode its exported csv to utf-8 twice
