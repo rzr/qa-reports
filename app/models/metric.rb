f02299c1321a513bd6c5d374f4c45846b826527f
Vesa Poikajärvi <vesa.poikajarvi@leonidasoy.fi>
2013-02-28 14:51:02 +0200

    Renamed model

--- app/models/metric.rb
+++ app/models/metric.rb
@@ -1,4 +1,4 @@
-class Metrics < ActiveRecord::Base
+class Metric < ActiveRecord::Base
   attr_accessible :group_name, :meego_test_session_id, :name, :unit, :value
 
   belongs_to :meego_test_session
