60470b2bdc7d7001d952ecf38c03dc2591f8ad2a
Vesa Poikajärvi <vesa.poikajarvi@leonidasoy.fi>
2013-03-01 12:23:51 +0200

    Test for hiding metrics via the API as well

--- features/step_definitions/api_steps.rb
+++ features/step_definitions/api_steps.rb
@@ -39,6 +39,12 @@ When "the client sends a basic test result file with option to hide summary" do
   @response = api_import @default_api_opts.merge({"hide_summary" => true})
 end
 
+When "the client sends a test result with metrics with option to hide metrics" do
+  @response = api_import @default_api_opts.merge({"hide_metrics" => true,
+    "result_files[]" => Rack::Test::UploadedFile.new("features/resources/xml_with_metrics.xml", "text/xml")})
+end
+
+
 # Note: this must use the API parameters for the current API version. There
 # are other methods for using deprecated parameters.
 When %r/^the client sends file "([^"]*)" via the REST API$/ do |file|
