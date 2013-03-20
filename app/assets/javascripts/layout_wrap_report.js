9b69481221fe5841c4311ea36df9a0cc6c6395fd
Vesa Poikajärvi <vesa.poikajarvi@leonidasoy.fi>
2013-03-20 14:39:52 +0200

    Combine scripts included in layout files
    Eventually target is to have only one javascript file served, and that's
    what Rails asset pipeline asset precompiling will by default do.

---
+++ app/assets/javascripts/layout_wrap_report.js
@@ -0,0 +1,7 @@
+//= require jquery.min
+//= require bluff/js-class
+//= require bluff/excanvas
+//= require bluff/bluff-min
+//= require dygraph-combined
+//= require string
+//= require jquery-ui-1.9.1.custom.min
