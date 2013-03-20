fb7f729e8a852137f86aa7a6968c80a56adba514
Vesa Poikajärvi <vesa.poikajarvi@leonidasoy.fi>
2013-03-20 08:50:58 +0200

    Possibility to inline image attachments
    Added new option to config.yml with which admin can choose to show
    image attachments inlined (i.e. using img tags) instead of just a link.

--- config/initializers/app_config.rb
+++ config/initializers/app_config.rb
@@ -2,6 +2,7 @@ APP_CONFIG = YAML.load_file("#{::Rails.root.to_s}/config/config.yml")
 
 APP_CONFIG['allow_empty_files']  ||= false
 APP_CONFIG['hide_empty_targets'] ||= false
+APP_CONFIG['inline_images']      ||= false
 APP_CONFIG['custom_results']     ||= []
 APP_CONFIG['app_name']           ||= "MeeGo QA Reports"
 APP_CONFIG['custom_css']         ||= ''
