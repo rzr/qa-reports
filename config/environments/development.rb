4f26224b9299077028ef4e5cb9da251fc045f9ed
Vesa Poikajärvi <vesa.poikajarvi@leonidasoy.fi>
2013-03-25 19:34:24 +0200

    Enable asset debug mode in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,5 +26,7 @@ Meegoqa::Application.configure do
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
 
+  # Serve asset files separately in development
+  config.assets.debug = true
 end
 
