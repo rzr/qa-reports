399773859a9b5470acdb84327462dd2e89a5034b
Vesa Poikajärvi <vesa.poikajarvi@leonidasoy.fi>
2013-03-27 09:36:46 +0200

    Moved Bugzilla code to a service module
    Since eventually we're going to a) need to fetch bug info from multiple
    Bugzilla servers and b) supporting also other services than Bugzilla,
    moved the code communicating with Bugzilla from controller to a module.

--- app/controllers/bugs_controller.rb
+++ app/controllers/bugs_controller.rb
@@ -1,6 +1,5 @@
 
-require "erb"
-include ERB::Util
+
 
 class BugsController < ApplicationController
 
@@ -9,66 +8,13 @@ class BugsController < ApplicationController
                 :expires_in => 1.hour
 
   def fetch_bugzilla_data
-    ids       = params[:bugids]
+    ids  = params[:bugids]
+    json = Bugzilla.fetch_data(SERVICES[0], ids)
 
-    uri = SERVICES[0]['uri'] + ids.join(',')
-
-    content = ""
-    if not SERVICES[0]['proxy_server'].nil?
-      @http = Net::HTTP.Proxy(SERVICES[0]['proxy_server'], SERVICES[0]['proxy_port']).new(SERVICES[0]['server'], SERVICES[0]['port'])
-    else
-      @http = Net::HTTP.new(SERVICES[0]['server'], SERVICES[0]['port'])
-    end
-
-    @http.use_ssl     = SERVICES[0]['use_ssl']
-    @http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-    @http.start() do |http|
-
-      cookie = nil
-      # Bugzilla integrated authentication
-      if not SERVICES[0]['bugzilla_username'].nil?
-        # Since Bugzilla 3.6 username and password can be given as parameters
-        login_uri = "#{uri}&Bugzilla_login=#{url_encode(SERVICES[0]['bugzilla_username'])}&Bugzilla_password=#{url_encode(SERVICES[0]['bugzilla_password'])}"
-
-        req = Net::HTTP::Get.new(login_uri)
-        if not SERVICES[0]['http_username'].nil?
-          req.basic_auth SERVICES[0]['http_username'], SERVICES[0]['http_password']
-        end
-
-        response = http.request(req)
-        cookie   = response.to_hash['set-cookie'].collect{|ea|ea[/^.*?;/]}.join(" ")
-
-        # If bugzilla set us a redirect URI, use that instead of the original one
-        if response.header['location']
-          uri = response.header['location']
-        end
-      end
-
-      req = Net::HTTP::Get.new(uri)
-      if not SERVICES[0]['http_username'].nil?
-        req.basic_auth SERVICES[0]['http_username'], SERVICES[0]['http_password']
-      end
-
-      if not cookie.nil?
-        req['Cookie'] = cookie
-      end
-
-      response = http.request(req)
-      if (charset = /charset=(.*)$/.match(response.header['content-type']).try(:captures)).try(:length) == 1
-        response.body.force_encoding(charset[0])
-      end
-      content  = response.body
-    end
-
-    begin
-      json = CSV.parse(content)
-      # TODO: Should render json instead of CSV
-      render :json => json
-    rescue CSV::MalformedCSVError => e
-      logger.error e.message
-      logger.info  "ERROR: MALFORMED BUGZILLA DATA"
-      logger.info  content
+    if json.nil?
       head :not_found
+    else
+      render :json => json
     end
   end
 
---
+++ app/services/bugzilla.rb
@@ -0,0 +1,70 @@
+require "erb"
+include ERB::Util
+
+# Fetch bug information from Bugzilla
+module Bugzilla
+
+  # Fetch bug information for given ids from given service (from
+  # external.services.yml configuration)
+  def self.fetch_data(service, ids)
+    uri = service['uri'] + ids.join(',')
+
+    content = ""
+    if not service['proxy_server'].nil?
+      @http = Net::HTTP.Proxy(service['proxy_server'], service['proxy_port']).new(service['server'], service['port'])
+    else
+      @http = Net::HTTP.new(service['server'], service['port'])
+    end
+
+    @http.use_ssl     = service['use_ssl']
+    @http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    @http.start() do |http|
+
+      cookie = nil
+      # Bugzilla integrated authentication
+      if not service['bugzilla_username'].nil?
+        # Since Bugzilla 3.6 username and password can be given as parameters
+        login_uri = "#{uri}&Bugzilla_login=#{url_encode(service['bugzilla_username'])}&Bugzilla_password=#{url_encode(service['bugzilla_password'])}"
+
+        req = Net::HTTP::Get.new(login_uri)
+        if not service['http_username'].nil?
+          req.basic_auth service['http_username'], service['http_password']
+        end
+
+        response = http.request(req)
+        cookie   = response.to_hash['set-cookie'].collect{|ea|ea[/^.*?;/]}.join(" ")
+
+        # If bugzilla set us a redirect URI, use that instead of the original one
+        if response.header['location']
+          uri = response.header['location']
+        end
+      end
+
+      req = Net::HTTP::Get.new(uri)
+      if not service['http_username'].nil?
+        req.basic_auth service['http_username'], service['http_password']
+      end
+
+      if not cookie.nil?
+        req['Cookie'] = cookie
+      end
+
+      response = http.request(req)
+      if (charset = /charset=(.*)$/.match(response.header['content-type']).try(:captures)).try(:length) == 1
+        response.body.force_encoding(charset[0])
+      end
+      content  = response.body
+    end
+
+    begin
+      json = CSV.parse(content)
+    rescue CSV::MalformedCSVError => e
+      logger.error e.message
+      logger.info  "ERROR: MALFORMED BUGZILLA DATA"
+      logger.info  content
+      json = nil
+    end
+
+    json
+  end
+end
