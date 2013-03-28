e54a8111f2aba15f5f7ede1500d09ceca089cec7
Vesa Poikajärvi <vesa.poikajarvi@leonidasoy.fi>
2013-03-28 10:40:47 +0200

    Correct IDs and URI for report JSON view
    Return the full bug ID (prefix included) as well as a link to the
    correct service when requesting a JSON for a test report.

--- app/models/meego_test_case.rb
+++ app/models/meego_test_case.rb
@@ -97,8 +97,12 @@ class MeegoTestCase < ActiveRecord::Base
       result: result_name,
       comment: comment_html,
       tc_id: tc_id.present? ? tc_id : nil,
-      # TODO: Fix to handle multiple services
-      bugs: comment.scan(/\[\[(\d+)\]\]/).map {|m| {id:m[0], url:"#{SERVICES[0]['link_uri']}#{m[0]}"}}
+      bugs: comment.scan(/\[\[((?:[A-Z]+\#{1})?\d+)\]\]/).map {|m|
+        {
+          id:  m[0],
+          url: ExternalServiceHelper.get_external_url(m[0])
+        }
+      }
     }
   end
 
